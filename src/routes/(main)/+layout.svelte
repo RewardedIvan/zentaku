f23b6b528035db3f353d159b8da72c7d87c95638
int4_t <rewardedivan@gmail.com>
2024-12-31 06:32:28 +0200

    feat(navbar): going back

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
 	import Menu from "$lib/ui/Menu.svelte";
 	import Ripple from "$lib/ui/Ripple.svelte";
-	import { MenuItem, CircularProgressIndeterminate } from "m3-svelte";
+	import { MenuItem, CircularProgressIndeterminate, Button, Icon } from "m3-svelte";
 	import { getProfile } from "$lib/anilist";
+	import ArrowBackIcon from "@ktibow/iconset-material-symbols/arrow-back";
+	import { page } from "$app/state";
 
 	const { children } = $props();
 
 	let profileMenuOpen = $state(false);
+
+	function goBack() {
+		window.history.back();
+	}
 </script>
 
 <div class="flex flex-col h-screen">
-	<div class="flex flex-row items-center justify-between bg-surface-container-highest p-2">
-		<div>
-			<h1>otakuzen</h1>
+	<div class="flex flex-row items-center justify-between bg-surface-container-highest p-2 h-[40px]">
+		<div class="flex flex-row gap-2 items-center">
+			{#if page.url.pathname !== "/"}
+				<Button on:click={goBack} iconType="full" type="text"><Icon icon={ArrowBackIcon} /></Button>
+			{/if}
+			<span class="font-afacad-flux text-xl">otakuzen</span>
 		</div>
 		<div class="flex flex-row items-center justify-center">
 			{#await getProfile()}
