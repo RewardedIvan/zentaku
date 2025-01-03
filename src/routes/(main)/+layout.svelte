4ead770e451594c18ca5d0d254a1dad899ed20dc
int4_t <rewardedivan@gmail.com>
2025-01-03 03:43:44 +0200

    feat(top bar): custom window decorations

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -3,20 +3,43 @@
 	import Ripple from "$lib/ui/Ripple.svelte";
 	import { MenuItem, CircularProgressIndeterminate, Button, Icon } from "m3-svelte";
 	import { getProfile } from "$lib/anilist";
-	import ArrowBackIcon from "@ktibow/iconset-material-symbols/arrow-back";
 	import { page } from "$app/state";
+	import { getCurrentWindow } from "@tauri-apps/api/window";
+
+	import ArrowBackIcon from "@ktibow/iconset-material-symbols/arrow-back";
+	import MaximizeIcon from "@ktibow/iconset-material-symbols/open-in-full-rounded";
+	import MinimizeIcon from "@ktibow/iconset-material-symbols/arrow-drop-down-rounded";
+	import XIcon from "@ktibow/iconset-material-symbols/close-rounded";
 
 	const { children } = $props();
 
 	let profileMenuOpen = $state(false);
+	let win = getCurrentWindow();
 
 	function goBack() {
 		window.history.back();
 	}
+
+	let dragDiv: HTMLDivElement | null = $state(null);
+	async function dragMouseDown(e: MouseEvent) {
+		if (e.target !== dragDiv) return;
+
+		e.preventDefault();
+		if (e.detail == 2) {
+			await win.toggleMaximize();
+		} else {
+			await win.startDragging();
+		}
+	}
 </script>
 
 <div class="flex flex-col h-screen">
-	<div class="flex flex-row items-center justify-between bg-surface-container-highest p-2 h-[40px]">
+	<div
+		class="flex flex-row items-center justify-between bg-surface-container-highest p-2 h-[40px] sticky top-0 z-[99999]"
+		onmousedown={dragMouseDown}
+		bind:this={dragDiv}
+		role="menubar" tabindex="0"
+	>
 		<div class="flex flex-row gap-2 items-center">
 			{#if page.url.pathname !== "/"}
 				<Button on:click={goBack} iconType="full" type="text"><Icon icon={ArrowBackIcon} /></Button>
@@ -39,6 +62,12 @@
 					{/snippet}
 				</Menu>
 			{/await}
+
+			<div class="flex flex-row ml-4">
+				<Ripple onClick={win.minimize}><Icon icon={MinimizeIcon} width="32" height="32" /></Ripple>
+				<Ripple onClick={win.toggleMaximize}><Icon icon={MaximizeIcon} width="32" height="32" /></Ripple>
+				<Ripple onClick={win.close}><Icon icon={XIcon} width="32" height="32" /></Ripple>
+			</div>
 		</div>
 	</div>
 
