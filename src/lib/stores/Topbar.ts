a0fbf34d85f97fa431d2330e47659c56101315f5
int4_t <rewardedivan@gmail.com>
2025-01-06 18:14:16 +0200

    feat: topbar hiding and fullscreen permissions

---
+++ src/lib/stores/Topbar.ts
@@ -0,0 +1,5 @@
+import { writable } from "svelte/store";
+
+export const Topbar = writable({
+	hidden: false,
+});
--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import Menu from "$lib/ui/Menu.svelte";
 	import Ripple from "$lib/ui/Ripple.svelte";
-	import { MenuItem, CircularProgressIndeterminate, Button, Icon } from "m3-svelte";
+	import {
+		MenuItem,
+		CircularProgressIndeterminate,
+		Button,
+		Icon,
+		easeEmphasizedDecel,
+		easeEmphasizedAccel,
+	} from "m3-svelte";
 	import { getProfile } from "$lib/anilist";
 	import { page } from "$app/state";
 	import { getCurrentWindow } from "@tauri-apps/api/window";
+	import { slide } from "svelte/transition";
+	import { Topbar } from "$lib/stores/Topbar";
 
 	import ArrowBackIcon from "@ktibow/iconset-material-symbols/arrow-back";
 	import MaximizeIcon from "@ktibow/iconset-material-symbols/open-in-full-rounded";
@@ -34,42 +43,52 @@
 </script>
 
 <div class="flex flex-col h-screen">
-	<div
-		class="flex flex-row items-center justify-between bg-surface-container-highest p-2 h-[40px] sticky top-0 z-[99999]"
-		onmousedown={dragMouseDown}
-		bind:this={dragDiv}
-		role="menubar" tabindex="0"
-	>
-		<div class="flex flex-row gap-2 items-center">
-			{#if page.url.pathname !== "/"}
-				<Button on:click={goBack} iconType="full" type="text"><Icon icon={ArrowBackIcon} /></Button>
-			{/if}
-			<span class="font-afacad-flux text-xl">otakuzen</span>
-		</div>
-		<div class="flex flex-row items-center justify-center">
-			{#await getProfile()}
-				<CircularProgressIndeterminate />
-			{:then profile}
-				<Menu bind:open={profileMenuOpen}>
-					<Ripple onClick={() => (profileMenuOpen = !profileMenuOpen)}>
-						<img src={profile.avatar} alt="avatar" width={32} draggable={false} />
-					</Ripple>
+	{#if !$Topbar.hidden}
+		<div
+			class="flex flex-row items-center justify-between bg-surface-container-highest p-2 h-[40px] sticky top-0 z-[99999]"
+			onmousedown={dragMouseDown}
+			bind:this={dragDiv}
+			role="menubar"
+			tabindex="0"
+			in:slide={{ duration: 300, easing: easeEmphasizedDecel }}
+			out:slide={{ duration: 300, easing: easeEmphasizedAccel }}
+		>
+			<div class="flex flex-row gap-2 items-center">
+				{#if page.url.pathname !== "/"}
+					<Button on:click={goBack} iconType="full" type="text"
+						><Icon icon={ArrowBackIcon} /></Button
+					>
+				{/if}
+				<span class="font-afacad-flux text-xl">otakuzen</span>
+			</div>
+			<div class="flex flex-row items-center justify-center">
+				{#await getProfile()}
+					<CircularProgressIndeterminate />
+				{:then profile}
+					<Menu bind:open={profileMenuOpen}>
+						<Ripple onClick={() => (profileMenuOpen = !profileMenuOpen)}>
+							<img src={profile.avatar} alt="avatar" width={32} draggable={false} />
+						</Ripple>
 
-					{#snippet menu()}
-						<MenuItem>Profile</MenuItem>
-						<MenuItem>Settings</MenuItem>
-						<MenuItem>Logout</MenuItem>
-					{/snippet}
-				</Menu>
-			{/await}
+						{#snippet menu()}
+							<MenuItem>Profile</MenuItem>
+							<MenuItem>Settings</MenuItem>
+							<MenuItem>Logout</MenuItem>
+						{/snippet}
+					</Menu>
+				{/await}
 
-			<div class="flex flex-row ml-4">
-				<Ripple onClick={win.minimize}><Icon icon={MinimizeIcon} width="32" height="32" /></Ripple>
-				<Ripple onClick={win.toggleMaximize}><Icon icon={MaximizeIcon} width="32" height="32" /></Ripple>
-				<Ripple onClick={win.close}><Icon icon={XIcon} width="32" height="32" /></Ripple>
+				<div class="flex flex-row ml-4">
+					<Ripple onClick={win.minimize}><Icon icon={MinimizeIcon} width="32" height="32" /></Ripple
+					>
+					<Ripple onClick={win.toggleMaximize}
+						><Icon icon={MaximizeIcon} width="32" height="32" /></Ripple
+					>
+					<Ripple onClick={win.close}><Icon icon={XIcon} width="32" height="32" /></Ripple>
+				</div>
 			</div>
 		</div>
-	</div>
+	{/if}
 
 	{@render children()}
 </div>
