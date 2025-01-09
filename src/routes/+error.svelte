1a8ad4d31fda41a527476d895d280b374d76edbc
int4_t <rewardedivan@gmail.com>
2025-01-09 19:32:08 +0200

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Button, Card } from "m3-svelte";
+
+	import { page } from "$app/state";
+	import { goto } from "$app/navigation";
+</script>
+
+<div class="flex flex-col items-center gap-2">
+    <Card type="filled">{page.status} {page.error?.message}</Card>
+
+    <div class="flex flex-row gap-1">
+        <Button type="text" on:click={() => window.history.back()}>Go back?</Button>
+        <Button type="text" on:click={() => goto("/")}>Go home?</Button>
+    </div>
+</div>
