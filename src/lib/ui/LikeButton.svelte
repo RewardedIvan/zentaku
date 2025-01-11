7327a9f7d30265078cdc0bfc9e563a78d4bc1875
int4_t <rewardedivan@gmail.com>
2025-01-11 13:52:10 +0200

    fix: better like button animation
    and format the file

--- src/lib/ui/LikeButton.svelte
+++ src/lib/ui/LikeButton.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-    import { Button, easeEmphasizedAccel, easeEmphasizedDecel } from "m3-svelte";
+	import { Button, easeEmphasizedAccel, easeEmphasizedDecel } from "m3-svelte";
 	import { type IconifyIcon } from "@iconify/types";
 	import { fly, scale, slide } from "svelte/transition";
 
-    import HeartIcon from "@ktibow/iconset-material-symbols/favorite";
-    import HeartOutlinedIcon from "@ktibow/iconset-material-symbols/favorite-outline";
-    import ThumbsUpIcon from "@ktibow/iconset-material-symbols/thumb-up-rounded";
-    import ThumbsDownIcon from "@ktibow/iconset-material-symbols/thumb-down-rounded";
+	import HeartIcon from "@ktibow/iconset-material-symbols/favorite";
+	import HeartOutlinedIcon from "@ktibow/iconset-material-symbols/favorite-outline";
+	import ThumbsUpIcon from "@ktibow/iconset-material-symbols/thumb-up-rounded";
+	import ThumbsDownIcon from "@ktibow/iconset-material-symbols/thumb-down-rounded";
 
-    interface Props {
-        value: boolean;
-        onClick: () => void;
-        icon: "thumb" | "heart";
-    }
+	interface Props {
+		value: boolean;
+		onClick: () => void;
+		icon: "thumb" | "heart";
+	}
 
-    let { value = $bindable(), icon, onClick }: Props = $props();
+	let { value = $bindable(), icon, onClick }: Props = $props();
 
-    /*
+	/*
             -- ascend --
             in:fly={{ delay: 150, y: 10 }}
             out:fly={{ duration: 300, y: -10 }}
@@ -46,36 +46,37 @@
             out:slide={{ axis: "x" }}
     */
 
-    function click() {
-        value = !value;
-        onClick();
-    }
+	function click() {
+		value = !value;
+		onClick();
+	}
 </script>
 
 {#snippet AnimatedIcon(icon: IconifyIcon)}
-    <svg
-        class="absolute"
-        width={icon.width} height={icon.height}
-        viewBox="0 0 {icon.width} {icon.height}"
-        in:fly={{ delay: 300, y: 40, easing: easeEmphasizedDecel }}
-        out:fly={{ duration: 300, y: -40, easing: easeEmphasizedAccel }}
-    >
-        {@html icon.body}
-    </svg>
+	<svg
+		class="absolute"
+		width={icon.width}
+		height={icon.height}
+		viewBox="0 0 {icon.width} {icon.height}"
+		in:scale={{ start: 0.1 }}
+		out:scale={{ duration: 300, start: 0.1 }}
+	>
+		{@html icon.body}
+	</svg>
 {/snippet}
 
 <Button type="text" iconType="full" on:click={click}>
-    {#if icon == "thumb"}
-        {#if value}
-            {@render AnimatedIcon(ThumbsUpIcon)}
-        {:else}
-            {@render AnimatedIcon(ThumbsDownIcon)}
-        {/if}
-    {:else if icon == "heart"}
-        {#if value}
-            {@render AnimatedIcon(HeartIcon)}
-        {:else}
-            {@render AnimatedIcon(HeartOutlinedIcon)}
-        {/if}
-    {/if}
+	{#if icon == "thumb"}
+		{#if value}
+			{@render AnimatedIcon(ThumbsUpIcon)}
+		{:else}
+			{@render AnimatedIcon(ThumbsDownIcon)}
+		{/if}
+	{:else if icon == "heart"}
+		{#if value}
+			{@render AnimatedIcon(HeartIcon)}
+		{:else}
+			{@render AnimatedIcon(HeartOutlinedIcon)}
+		{/if}
+	{/if}
 </Button>
