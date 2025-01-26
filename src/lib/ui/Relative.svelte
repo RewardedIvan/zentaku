ff9772334163c1341c6e235c764c4f78b067bd29
int4_t <rewardedivan@gmail.com>
2025-01-26 10:59:25 +0200

    feat(relative): props for shorter dates and truncating

--- src/lib/ui/Relative.svelte
+++ src/lib/ui/Relative.svelte
@@ -5,21 +5,43 @@
 	interface Props {
 		futureDate?: Date;
 		futureUnix?: number;
+		short?: boolean;
+		truncate?: number;
 	}
 
 	let text = $state("");
 
-	let { futureDate, futureUnix }: Props = $props();
+	let { futureDate, futureUnix, short, truncate }: Props = $props();
 
-	let intv = setInterval(() => {
+	function update() {
 		if (!futureDate && !futureUnix) {
 			return;
 		}
 
-		let date = futureDate ?? fromUnixTime(futureUnix ?? 0);
-		text = formatDuration(intervalToDuration(interval(new Date(), date)));
-	}, 1000);
+		const dur = intervalToDuration(
+			interval(new Date(), futureDate ?? fromUnixTime(futureUnix ?? 0)),
+		);
 
+		if (short) {
+			text = Object.entries(dur)
+				.map(([k, v]) => {
+					if (v > 0) {
+						return `${v}${k[0]}`;
+					}
+				})
+				.join(", ");
+		} else {
+			text = formatDuration(dur);
+		}
+
+		if (truncate) {
+			text = text.substring(0, truncate);
+		}
+	}
+
+	update();
+
+	let intv = setInterval(update, 1000);
 	onDestroy(() => {
 		clearInterval(intv);
 	});
