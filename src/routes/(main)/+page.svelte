44bca805e6e39794d5829c46090a7e8ae987432b
int4_t <rewardedivan@gmail.com>
2025-04-20 21:47:20 +0300

    feat(home): add tabs for manga and feed

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,19 +1,61 @@
 <script lang="ts">
-	import { MenuItem, CircularProgressIndeterminate } from "m3-svelte";
+	import { MenuItem, CircularProgressIndeterminate, NavList, NavListButton } from "m3-svelte";
 	import { getProfile, getMediaLists } from "$lib/anilist";
 	import MediaList from "./MediaList.svelte";
+	import type { IconifyIcon } from "@iconify/types";
+	import BookIcon from "@ktibow/iconset-material-symbols/book";
+	import BookIconOutline from "@ktibow/iconset-material-symbols/book-outline";
+	import AnimeIcon from "@ktibow/iconset-material-symbols/movie";
+	import AnimeIconOutline from "@ktibow/iconset-material-symbols/movie-outline";
+	import FeedIcon from "@ktibow/iconset-material-symbols/home";
+	import FeedIconOutline from "@ktibow/iconset-material-symbols/home-outline";
+
+	let tab: "anime" | "manga" | "feed" = "anime";
 </script>
 
-<div class="flex flex-grow flex-col">
-	{#await getMediaLists("anime")}
-		<CircularProgressIndeterminate />
-	{:then mediaLists}
-		{#if mediaLists}
-			{#each mediaLists as list}
-				<MediaList {list} />
-			{/each}
+<div class="flex flex-row h-full w-full overflow-hidden gap-4">
+	{#snippet _tab(name: typeof tab, icon: IconifyIcon, iconS: IconifyIcon)}
+		{@const selected = tab == name}
+		<NavListButton
+			type="rail"
+			{selected}
+			icon={selected ? iconS : icon}
+			on:click={() => (tab = name)}
+		>
+			{name}
+		</NavListButton>
+	{/snippet}
+
+	<NavList
+		type="rail"
+		extraOptions={{
+			style: "display: flex; padding-left: 1rem; padding-right: 1rem; height: 100%;",
+		}}
+	>
+		{@render _tab("anime", AnimeIcon, AnimeIconOutline)}
+		{@render _tab("manga", BookIcon, BookIconOutline)}
+		{@render _tab("feed", FeedIcon, FeedIconOutline)}
+	</NavList>
+
+	<div class="flex flex-grow flex-col overflow-auto min-w-full">
+		{#if tab == "feed"}
+			<!-- TODO -->
 		{:else}
-			<span class="text-center">No lists</span>
+			{#await getMediaLists(tab)}
+				<div class="flex justify-center items-center h-full">
+					<CircularProgressIndeterminate />
+				</div>
+			{:then mediaLists}
+				{#if mediaLists}
+					{#each mediaLists as list}
+						<MediaList {list} />
+					{/each}
+				{:else}
+					<div class="flex justify-center items-center h-full">
+						<span class="text-center">No lists</span>
+					</div>
+				{/if}
+			{/await}
 		{/if}
-	{/await}
+	</div>
 </div>
