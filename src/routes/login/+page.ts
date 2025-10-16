9153ae1526a58f5ed4d1f5b98529370bcd346bda
Aspect <236003979+givemecursorsubscription@users.noreply.github.com>
2025-10-15 18:27:53 -0700

    remove the unnessecary bloat in the project

--- src/routes/login/+page.ts
+++
@@ -1,8 +0,0 @@
-import { invoke } from "@tauri-apps/api/core";
-import type { PageLoad } from "./$types";
-
-export const load: PageLoad = async () => {
-	return {
-		loggedIn: await invoke<boolean>("check_login"),
-	};
-};
